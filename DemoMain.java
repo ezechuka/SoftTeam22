5842133368197c4258fd567069db251130eca491
ezechuka <ezechukwuka9@gmail.com>
2021-07-12 03:04:58 +0100

    UPDATED Added .gitignore and removed .class from staging

--- DemoMain.java
+++ DemoMain.java
@@ -6,6 +6,11 @@ public class DemoMain {
 	public static void main(String[] args) {
 		// Declare and initialize a DemoUseInterfaceCar object
 		DemoUseInterfaceCar demoCar = new DemoUseInterfaceCar(7, 6.65);
+		
+		
+		
+		
+		
 
 		// call getter methods of demoCar object
 		int numberOfPassengers = demoCar.getNumberPassengers();
